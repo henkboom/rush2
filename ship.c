8ad02ffefd45d564334cf72a52349be1ab84b2ba
Henk Boom <henk@henk.ca>
2011-02-15 02:43:04 -0500

    ship height physics

--- ship.c
+++ ship.c
@@ -4,6 +4,7 @@
 
 #include "rhizome/quaternion.h"
 #include "rhizome/vect.h"
+#include "noise.h"
 
 ship_h add_ship_component(game_context_s *context, component_h parent)
 {
@@ -53,11 +54,31 @@ static void handle_tick(game_context_s *context, void *data, const nothing_s *n)
 
     const transform_s *transform = handle_get(ship->transform);
 
-    ship->velocity = vect_add(ship->velocity,
+    // in ground?
+    double height =
+        transform->pos.y - noise_generator_sample_at(transform->pos) - 0.2;
+    if(height <= 0)
+    {
+        vect_s normal = noise_generator_normal_at(transform->pos);
+        if(vect_dot(normal, ship->velocity) < 0)
+        {
+            ship->velocity = vect_sub(
+                ship->velocity,
+                vect_div(vect_project(ship->velocity, normal), 4));
+        }
+    }
+
+    ship->velocity = vect_add(
+        ship->velocity,
         vect_mul(quaternion_rotate_i(transform->orientation), control.y/50));
+    ship->velocity = vect_add(ship->velocity, make_vect(0, -0.5/60, 0));
     // damp
     ship->velocity = vect_mul(ship->velocity, 0.98);
 
     send_transform_rotate(context, transform->component, rotation);
-    send_transform_move(context, transform->component, ship->velocity);
+
+    vect_s movement = ship->velocity;
+    if(height < 0)
+        movement = vect_add(movement, make_vect(0, -height, 0));
+    send_transform_move(context, transform->component, movement);
 }
