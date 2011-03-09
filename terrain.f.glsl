29b8d3a3ab660d5d8b3fb0786f803cb808c01df1
Henk Boom <henk@henk.ca>
2011-03-09 18:47:01 -0500

    pixel shader contrast improvements, windows

--- terrain.f.glsl
+++ terrain.f.glsl
@@ -10,11 +10,14 @@ void main()
 
     if(frag_on_edge > 1.0 - width) {
         gl_FragColor.r = color;
+    } else if(frag_on_edge > 1.0 - width*10){
+        gl_FragColor.r = 0.0;
     } else {
         gl_FragColor.r = 0.0;
+        discard;
     }
     gl_FragColor.gb = gl_FragColor.rr;
     gl_FragColor.b = gl_FragColor.b/(frag_on_edge);
     gl_FragColor.r = gl_FragColor.r/(frag_on_edge+width/10);
-    gl_FragColor.a = 0.2 + gl_FragColor.r * 0.2;
+    gl_FragColor.a = 0.7;// 0.2 + gl_FragColor.r * 0.2;
 }
